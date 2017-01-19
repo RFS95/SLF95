9c747a65966ab1b4377004ece87f719b7038950f
odelia0098 <odelia2012@yahoo.com>
2017-01-19 22:02:59 +0330

    Add files via upload
    taghire form

---
+++ taghire form.c
@@ -0,0 +1,286 @@
+int taghire_form(int m)//sepide
+{
+  int i;   
+	
+		
+      if(m==1)	{FILE *fp = fopen("new form Persepolis .txt", "w");
+		if (!fp) {
+			perror("fopen");
+		return 0;
+                }
+    form_change(m);
+    for( i=1; i<= t[m].tedad ; i++)
+    {
+          	fprintf(fp,"%d %d %s %d %c %d %d %d\n",t[m].p[i].shomare,t[m].p[i].num,t[m].p[i].name,t[m].p[i].age,t[m].p[i].post,t[m].p[i].skill,t[m].p[i].fitness,t
+             [m].p[i].form);
+     } 
+     	fclose(fp);
+		fp = NULL;	}
+	
+	
+		
+		
+      else  if(m==2)	{FILE *fp = fopen("new form Tractorsazi .txt", "w");
+		if (!fp) {
+			perror("fopen");
+		return 0;
+                 }
+    form_change(m);
+    for( i=1; i<= t[m].tedad ; i++)
+    {
+          	fprintf(fp,"%d %d %s %d %c %d %d %d\n",t[m].p[i].shomare,t[m].p[i].num,t[m].p[i].name,t[m].p[i].age,t[m].p[i].post,t[m].p[i].skill,t[m].p[i].fitness,t
+             [m].p[i].form);
+     } 
+     	fclose(fp);
+		fp = NULL;	}
+       
+
+	
+	
+		
+      else  if(m==3)	{FILE *fp = fopen("new form Paykan .txt", "w");
+		if (!fp) {
+			perror("fopen");
+		return 0;
+                 }
+    form_change(m);
+    for( i=1; i<= t[m].tedad ; i++)
+    {
+          	fprintf(fp,"%d %d %s %d %c %d %d %d\n",t[m].p[i].shomare,t[m].p[i].num,t[m].p[i].name,t[m].p[i].age,t[m].p[i].post,t[m].p[i].skill,t[m].p[i].fitness,t
+             [m].p[i].form);
+     } 
+     	fclose(fp);
+		fp = NULL;	}
+       
+
+
+	
+		
+      else  if(m==4)	{FILE *fp = fopen("new form Zobahan .txt", "w");
+		if (!fp) {
+			perror("fopen");
+		return 0;
+                 }
+        form_change(m);
+    for( i=1; i<= t[m].tedad ; i++)
+    {
+          	fprintf(fp,"%d %d %s %d %c %d %d %d\n",t[m].p[i].shomare,t[m].p[i].num,t[m].p[i].name,t[m].p[i].age,t[m].p[i].post,t[m].p[i].skill,t[m].p[i].fitness,t
+             [m].p[i].form);
+     } 
+     	fclose(fp);
+		fp = NULL;	}
+       
+
+	
+		
+		
+      else  if(m==5)	{FILE *fp = fopen("new form Naft Tehran .txt", "w");
+		if (!fp) {
+			perror("fopen");
+		return 0;
+                 }
+        form_change(m);
+
+    for( i=1; i<= t[m].tedad ; i++)
+    {
+          	fprintf(fp,"%d %d %s %d %c %d %d %d\n",t[m].p[i].shomare,t[m].p[i].num,t[m].p[i].name,t[m].p[i].age,t[m].p[i].post,t[m].p[i].skill,t[m].p[i].fitness,t
+             [m].p[i].form);
+     } 
+     	fclose(fp);
+		fp = NULL;	}
+       
+
+
+		
+      else  if(m==6)	{FILE *fp = fopen("new form Foolad .txt", "w");
+		if (!fp) {
+			perror("fopen");
+		return 0;
+                 }
+        form_change(m);
+
+    for( i=1; i<= t[m].tedad ; i++)
+    {
+          	fprintf(fp,"%d %d %s %d %c %d %d %d\n",t[m].p[i].shomare,t[m].p[i].num,t[m].p[i].name,t[m].p[i].age,t[m].p[i].post,t[m].p[i].skill,t[m].p[i].fitness,t
+             [m].p[i].form);
+     } 
+     	fclose(fp);
+		fp = NULL;	}
+       
+
+		
+      else  if(m==7)	{FILE *fp = fopen("new form Esteghlal .txt", "w");
+		if (!fp) {
+			perror("fopen");
+		return 0;
+                 }
+    form_change(m);
+
+    for( i=1; i<= t[m].tedad ; i++)
+    {
+          	fprintf(fp,"%d %d %s %d %c %d %d %d\n",t[m].p[i].shomare,t[m].p[i].num,t[m].p[i].name,t[m].p[i].age,t[m].p[i].post,t[m].p[i].skill,t[m].p[i].fitness,t
+             [m].p[i].form);
+     } 
+     	fclose(fp);
+		fp = NULL;	}
+       
+
+		
+      else  if(m==8)	{FILE *fp = fopen("new form Sepahan .txt", "w");
+		if (!fp) {
+			perror("fopen");
+		return 0;
+                 }
+        form_change(m);
+
+    for( i=1; i<= t[m].tedad ; i++)
+    {
+          	fprintf(fp,"%d %d %s %d %c %d %d %d\n",t[m].p[i].shomare,t[m].p[i].num,t[m].p[i].name,t[m].p[i].age,t[m].p[i].post,t[m].p[i].skill,t[m].p[i].fitness,t
+             [m].p[i].form);
+     } 
+     	fclose(fp);
+		fp = NULL;	}
+       
+
+
+		
+      else  if(m==9)	{FILE *fp = fopen("new form Esteghlal khuzestan .txt", "w");
+		if (!fp) {
+			perror("fopen");
+		return 0;
+                 }
+        form_change(m);
+
+    for( i=1; i<= t[m].tedad ; i++)
+    {
+          	fprintf(fp,"%d %d %s %d %c %d %d %d\n",t[m].p[i].shomare,t[m].p[i].num,t[m].p[i].name,t[m].p[i].age,t[m].p[i].post,t[m].p[i].skill,t[m].p[i].fitness,t
+             [m].p[i].form);
+     } 
+     	fclose(fp);
+		fp = NULL;	}
+       
+
+		
+      else  if(m==10)	{FILE *fp = fopen("new form Gostaresh Foolad .txt", "w");
+		if (!fp) {
+			perror("fopen");
+		return 0;
+                 }
+         form_change(m);
+
+    for( i=1; i<= t[m].tedad ; i++)
+    {
+          	fprintf(fp,"%d %d %s %d %c %d %d %d\n",t[m].p[i].shomare,t[m].p[i].num,t[m].p[i].name,t[m].p[i].age,t[m].p[i].post,t[m].p[i].skill,t[m].p[i].fitness,t
+             [m].p[i].form);
+     } 
+     	fclose(fp);
+		fp = NULL;	}
+       
+
+		
+      else  if(m==11)	{FILE *fp = fopen("new form Sanat Naft .txt", "w");
+		if (!fp) {
+			perror("fopen");
+		return 0;
+                 }
+         form_change(m);
+
+    for( i=1; i<= t[m].tedad ; i++)
+    {
+          	fprintf(fp,"%d %d %s %d %c %d %d %d\n",t[m].p[i].shomare,t[m].p[i].num,t[m].p[i].name,t[m].p[i].age,t[m].p[i].post,t[m].p[i].skill,t[m].p[i].fitness,t
+             [m].p[i].form);
+     } 
+     	fclose(fp);
+		fp = NULL;	}
+       
+
+
+		
+      else  if(m==12)	{FILE *fp = fopen("new form Padideh Khorasan .txt", "w");
+		if (!fp) {
+			perror("fopen");
+		return 0;
+                 }
+        form_change(m);
+
+    for( i=1; i<= t[m].tedad ; i++)
+    {
+          	fprintf(fp,"%d %d %s %d %c %d %d %d\n",t[m].p[i].shomare,t[m].p[i].num,t[m].p[i].name,t[m].p[i].age,t[m].p[i].post,t[m].p[i].skill,t[m].p[i].fitness,t
+             [m].p[i].form);
+     } 
+     	fclose(fp);
+		fp = NULL;	}
+       
+
+
+		
+      else  if(m==13)	{FILE *fp = fopen("new form Siahjamegan .txt", "w");
+		if (!fp) {
+			perror("fopen");
+		return 0;
+                 }
+       form_change(m);
+
+    for( i=1; i<= t[m].tedad ; i++)
+    {
+          	fprintf(fp,"%d %d %s %d %c %d %d %d\n",t[m].p[i].shomare,t[m].p[i].num,t[m].p[i].name,t[m].p[i].age,t[m].p[i].post,t[m].p[i].skill,t[m].p[i].fitness,t
+             [m].p[i].form);
+     } 
+     	fclose(fp);
+		fp = NULL;	}
+       
+
+		
+      else  if(m==14)	{FILE *fp = fopen("new form Saipa .txt", "w");
+		if (!fp) {
+			perror("fopen");
+		return 0;
+                 }
+        form_change(m);
+
+    for( i=1; i<= t[m].tedad ; i++)
+    {
+          	fprintf(fp,"%d %d %s %d %c %d %d %d\n",t[m].p[i].shomare,t[m].p[i].num,t[m].p[i].name,t[m].p[i].age,t[m].p[i].post,t[m].p[i].skill,t[m].p[i].fitness,t
+             [m].p[i].form);
+     } 
+     	fclose(fp);
+		fp = NULL;	}
+       
+
+
+		
+      else  if(m==15)	{FILE *fp = fopen("new form Saba Qom .txt", "w");
+		if (!fp) {
+			perror("fopen");
+		return 0;
+                 }
+        form_change(m);
+
+    for( i=1; i<= t[m].tedad ; i++)
+    {
+          	fprintf(fp,"%d %d %s %d %c %d %d %d\n",t[m].p[i].shomare,t[m].p[i].num,t[m].p[i].name,t[m].p[i].age,t[m].p[i].post,t[m].p[i].skill,t[m].p[i].fitness,t
+             [m].p[i].form);
+     } 
+     	fclose(fp);
+		fp = NULL;	}
+       
+
+
+		
+      else  if(m==16)	{FILE *fp = fopen("new form Machine Sazi .txt", "w");
+		if (!fp) {
+			perror("fopen");
+		return 0;
+                 }
+                     form_change(m);
+
+    for( i=1; i<= t[m].tedad ; i++)
+    {
+          	fprintf(fp,"%d %d %s %d %c %d %d %d\n",t[m].p[i].shomare,t[m].p[i].num,t[m].p[i].name,t[m].p[i].age,t[m].p[i].post,t[m].p[i].skill,t[m].p[i].fitness,t
+             [m].p[i].form);
+     } 
+     	fclose(fp);
+		fp = NULL;	}
+       
+
+	}
