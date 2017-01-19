9c2396ae9bbeac1deb34766ec74ba6bc4130e4c2
odelia0098 <odelia2012@yahoo.com>
2017-01-19 21:44:21 +0330

    Add files via upload
    Taghvim

---
+++ Untitled2.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+void taghvim(int week){
+	if (week==1)
+		printf("\n\t\t1395/6/26\tjome");
+	else if (week==2)
+	   	printf("\n\t\t1395/7/2\tjome");
+	else if (week==3)
+	   	printf("\n\t\t1395/7/9\tjome");
+ 	else if (week==4)
+	   	printf("\n\t\t1395/7/16\tjome");
+	else if (week==5)
+	   	printf("\n\t\t1395/7/23\tjome");
+	else if (week==6)
+	   	printf("\n\t\t1395/7/30\tjome");
+	else if (week==7)
+	   	printf("\n\t\t1395/8/7\tjome");
+	else if (week==8)
+	   	printf("\n\t\t1395/8/14\tjome");
+	else if (week==9)
+	   	printf("\n\t\t1395/8/21\tjome");
+	else if (week==10)
+	   	printf("\n\t\t1395/8/28\tjome");
+	else if (week==11)
+	   	printf("\n\t\t1395/9/5\tjome");
+ 	else if (week==12)
+	   	printf("\n\t\t1395/9/12\tjome");
+	else if (week==13)
+	   	printf("\n\t\t1395/9/19\tjome");
+	else if (week==14)
+	   	printf("\n\t\t1395/9/26\tjome");
+	else if (week==15)
+	   	printf("\n\t\t1395/10/3\tjome");
+	else if (week==16)
+	   	printf("\n\t\t1395/10/23\tpanjshanbe");
+	else if (week==17)
+	   	printf("\n\t\t1395/10/30\tpanjshanbe");
+	else if (week==18)
+	   	printf("\n\t\t1395/11/7\tpanjshanbe");
+	else if (week==19)
+	   	printf("\n\t\t1395/11/14\tpanjshanbe");
+ 	else if (week==20)
+	   	printf("\n\t\t1395/11/21\tpanjshanbe");
+	else if (week==21)
+	   	printf("\n\t\t1395/11/28\tpanjshanbe");
+	else if (week==22)
+	   	printf("\n\t\t1395/12/5\tpanjshanbe");
+	else if (week==23)
+	   	printf("\n\t\t1395/12/12\tpanjshanbe");
+	else if (week==24)
+	   	printf("\n\t\t1395/12/19\tpanjshanbe");
+	else if (week==25)
+	   	printf("\n\t\t1395/12/26\tpanjshanbe");
+	else if (week==26)
+	   	printf("\n\t\t1396/1/3\tpanjshanbe");
+	else if (week==27)
+	   	printf("\n\t\t1396/1/10\tpanjshanbe");
+ 	else if (week==28)
+	   	printf("\n\t\t1396/1/17\tpanjshanbe");
+	else if (week==29)
+	   	printf("\n\t\t1396/1/24\tpanjshanbe");
+	else  (week==30)
+	   	printf("\n\t\t1396/1/31\tpanjshanbe");
+	 	   	   	   	   	   	      	
+}
